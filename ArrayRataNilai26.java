bcc9d83aa56caeae61dc8d0f0b353e220786d004
SeptianTito123 <SeptianTito123>
2023-10-27 19:41:16 +0700

    Array1

--- ArrayRataNilai26.java
+++ ArrayRataNilai26.java
@@ -2,17 +2,33 @@ import java.util.Scanner;
 public class ArrayRataNilai26 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[] nilaiMhs= new int[10];
-        double total = 0;
-        double rata2;
+
+        System.out.println("Masukkan jumlah mahasiswa = ");
+        int jumlahMhs = input.nextInt();
+        int[] nilaiMhs= new int[jumlahMhs];
+        double totalLulus = 0;
+        double totalTidakLulus = 0;
+        double rataLulus;
+        double rataTidakLulus;
+        int jumlahLulus = 0;
+        int jumlahTidakLulus = 0;
         
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" +(i+1)+ " : ");
             nilaiMhs[i] = input.nextInt();
         } for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                totalLulus += nilaiMhs[i];
+                jumlahLulus++;
+            } else {
+                totalTidakLulus += nilaiMhs[i];
+                jumlahTidakLulus++;
+            }
         }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = " +rata2);
+        rataLulus = (jumlahLulus > 0) ? totalLulus / jumlahLulus : 0;
+        rataTidakLulus = (totalTidakLulus > 0) ? totalLulus / jumlahTidakLulus :0;
+
+        System.out.println("Rata-rata nilai lulus = " +rataLulus);
+        System.out.println("Jumlah mahasiswa yang lulus = " +jumlahLulus);
     }
 }
