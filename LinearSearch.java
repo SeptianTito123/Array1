62426992abc913d663b874b23541ed033c5e8878
SeptianTito123 <SeptianTito123>
2023-10-27 20:20:37 +0700

    Array1

---
+++ LinearSearch.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class LinearSearch {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan jumlah elemen array :");
+        
+        int jumlahElemen = input.nextInt();
+        int[] arrayInt = new int[jumlahElemen];
+        int key;
+        int hasil = 0;
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.print("Masukkan elemen array ke-" +i+ " : ");
+            arrayInt[i] = input.nextInt();
+        }
+
+        System.out.print("Masukkan key yang ingin di cari :");
+        key = input.nextInt();
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (arrayInt[i] == key) {
+                hasil = i;
+                break;
+            }
+        }
+        if (hasil == -1) {
+            System.out.println("Key tidak ditemukan");
+        } else {
+            System.out.println("Key ada dalam array pada posisi indeks ke-" +hasil);
+        }
+    }
+}
